9dd24b5b21455f69a2b896a2548249791fc8808e
Pascal Leconte <pascal.leconte@conseilgouz.com>
2025-03-13 17:41:38 +0100

    3.1.0
    Use VisForms events

--- src/Extension/Cgsecure.php
+++ src/Extension/Cgsecure.php
@@ -1,37 +1,44 @@
 <?php
 /**
  * @component     Plugin Visforms CG Secure
- * Version			: 3.0.0
+ * Version			: 3.1.0
  * @license https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
- * @copyright (c) 2023 ConseilGouz. All Rights Reserved.
- * @author ConseilGouz 
+ * @copyright (c) 2025 ConseilGouz. All Rights Reserved.
+ * @author ConseilGouz
 **/
+
 namespace ConseilGouz\Plugin\Visforms\Cgsecure\Extension;
+
 // No direct access.
 defined('_JEXEC') or die();
 use Joomla\CMS\Plugin\CMSPlugin;
-use Joomla\CMS\Form\Form;
-use Joomla\CMS\Factory;
-use Joomla\CMS\Language\Text;
+use Joomla\Event\SubscriberInterface;
 use ConseilGouz\CGSecure\Helper\Cgipcheck;
+use Visolutions\Component\Visforms\Site\Event\Visforms\VisformsFormPrepareEvent;
 
-class Cgsecure extends CMSPlugin
+class Cgsecure extends CMSPlugin implements SubscriberInterface
 {
-    public $myname='VisFormsCGSecure';
-	public $mymessage='(visforms) : try to access forms...';
-	public $errtype = 'w';	 // warning 
-	public $cgsecure_params;
-	public function __construct(&$subject, $config)
-	{
-		parent::__construct($subject, $config);
-		$this->cgsecure_params = Cgipcheck::getParams();
-		$prefixe = $_SERVER['SERVER_NAME'];
-		$prefixe = substr(str_replace('www.','',$prefixe),0,2);
-		$this->mymessage = $prefixe.$this->errtype.'-'.$this->mymessage;
-	}
-	// Check IP on prepare Forms
-	function onVisformsFormPrepare($context,$form,$params)
-	{
-		Cgipcheck::check_ip($this,$this->myname);
-	}
+    public $myname = 'VisFormsCGSecure';
+    public $mymessage = '(visforms) : try to access forms...';
+    public $errtype = 'w';	 // warning
+    public $cgsecure_params;
+    public function __construct(&$subject, $config)
+    {
+        parent::__construct($subject, $config);
+        $this->cgsecure_params = Cgipcheck::getParams();
+        $prefixe = $_SERVER['SERVER_NAME'];
+        $prefixe = substr(str_replace('www.', '', $prefixe), 0, 2);
+        $this->mymessage = $prefixe.$this->errtype.'-'.$this->mymessage;
+    }
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'onVisformsFormPrepare' => 'onVisformsFormPrepare',
+        ];
+    }
+    // Check IP on prepare Forms
+    public function onVisformsFormPrepare(VisformsFormPrepareEvent $event): void
+    {
+        Cgipcheck::check_ip($this, $this->myname);
+    }
 }
